e0ed9a125496825a39d2597f73c9bcc3dce2b27d
Jose Roberto Fernandez Ticse <jose.fernandez.external@atos.net>
2021-06-11 00:09:47 +0200

    Deploy Netlify

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
+//local
 export const API_URL = "http://localhost:8180";
+//Deploy
+// export const API_URL = "https://eccommerce-vue.herokuapp.com";
+
 export const TOKEN = "token";
 export const PRODUCTS = "products";
